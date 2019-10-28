d298fdf6d5e8f3e9016bfe7337651d739a8af59e
1325075688gw <1325075688@qq.com>
2019-10-28 19:03:33 +0800

    Judge whether it is overload.

---
+++ day04-code/src/com/biggw/day04/demo03/Demo03MethodOverloadJudge.java
@@ -0,0 +1,18 @@
+package com.biggw.day04.demo03;
+
+/**
+ * @author gw
+ * @date 2019/10/28 0028 下午 17:51
+ */
+public class Demo03MethodOverloadJudge {
+/*    public static void open(){} // 正确重载
+    public static void open(int a){} // 正确重载
+    public void open(int a, int b){} // 错误，和11行冲突
+    public static void open(double a,int b){} // 正确
+    public static void open(int a,double b){} // 错误，和13行冲突
+    public void open(int a,double d){} // 错误，和12行冲突
+    public static void OPEN(){} // 正确
+    public static void open(int i, int j){} // 错误，和10行冲突
+
+*/
+}
