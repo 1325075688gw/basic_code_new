0566bf4ed040483169a10106a98e5218aa2b7b9c
1325075688gw <1325075688@qq.com>
2019-10-28 20:08:13 +0800

    The use of array.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

---
+++ day05-code/src/com/biggw/day05/demo01/Demo04ArrayUse.java
@@ -0,0 +1,23 @@
+package com.biggw.day05.demo01;
+
+/**
+ * @author gw
+ * @date 2019/10/28 0028 下午 19:59
+ */
+
+
+/*
+* 直接打印数组的名称，得到的是数组对应的，内存地址的Hash值 （16进制）
+* 
+* arrayA = [I@7530d0a
+* [ :表示数组
+* I：数组内容的类型为int
+*
+* */
+
+public class Demo04ArrayUse {
+    public static void main(String[] args) {
+        int[] arrayA = {1, 2, 3, 4};
+        System.out.println("arrayA = " + arrayA);
+    }
+}
