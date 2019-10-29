6f0abc749a206cc6e402fdd4dd260d0b20e203e4
1325075688gw <1325075688@qq.com>
2019-10-29 15:27:33 +0800

    The traverse of array.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

---
+++ day05-code/src/com/biggw/day05/demo03/Demo04ArrayTraverse.java
@@ -0,0 +1,15 @@
+package com.biggw.day05.demo03;
+
+/**
+ * @author gw
+ * @date 2019/10/29 0029 下午 15:24
+ */
+public class Demo04ArrayTraverse {
+    public static void main(String[] args) {
+        int[] arrayA = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < arrayA.length; i++) {
+            System.out.println("array[" + i + "]: " + arrayA[i]);
+        }
+    }
+}
