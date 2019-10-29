6495b80848af69488e0d2199496fd64939b313e8
1325075688gw <1325075688@qq.com>
2019-10-29 14:55:06 +0800

    The memory use of dynamic array.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

---
+++ day05-code/src/com/biggw/day05/demo02/Demo01ArrayOne.java
@@ -0,0 +1,21 @@
+package com.biggw.day05.demo02;
+
+/**
+ * @author gw
+ * @date 2019/10/28 0028 下午 23:45
+ */
+public class Demo01ArrayOne {
+    public static void main(String[] args) {
+        int[] arrayA = new int[3];
+        System.out.println("arrayA = " + arrayA);
+        System.out.println(arrayA[2]);
+
+
+        arrayA[2] = 2;
+        System.out.println("arrayA = " + arrayA);
+        System.out.println(arrayA[2]);
+
+        // TODO: 2019/10/29 0029 : 定义两个数组
+
+    }
+}
