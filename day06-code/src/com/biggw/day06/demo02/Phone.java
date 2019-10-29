9755db1074478293d3a39cdeb7ded7350b0436e5
1325075688gw <1325075688@qq.com>
2019-10-29 20:01:43 +0800

    Phone Class
    Signed-off-by: 1325075688gw <1325075688@qq.com>

--- day06-code/src/com/biggw/day06/demo02/Phone.java
+++ day06-code/src/com/biggw/day06/demo02/Phone.java
@@ -9,11 +9,22 @@ public class Phone {
     int price;
     String color;
 
-    public void call(String who){
+    public void call(String who) {
         System.out.println("打电话给： " + who);
     }
 
-    public void sendMessage(){
+    public void sendMessage() {
         System.out.println("群发短信");
+
+        /*
+         * 局部变量:没有默认值,如果要想使用,必须先手动赋值,然后再使用
+         * 成员变量、类变量:有默认值
+         * */
+
+
+        /*
+         * int a;
+         * System.out.println("a = " + a);
+         */
     }
 }
