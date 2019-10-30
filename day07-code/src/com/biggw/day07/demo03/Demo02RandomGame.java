9d1a3abdb6397f834248b65bcdb01c3eddecb221
1325075688gw <1325075688@qq.com>
2019-10-30 11:05:31 +0800

    GuessNum
    Signed-off-by: 1325075688gw <1325075688@qq.com>

--- day07-code/src/com/biggw/day07/demo03/Demo02RandomGame.java
+++ day07-code/src/com/biggw/day07/demo03/Demo02RandomGame.java
@@ -9,14 +9,44 @@ import java.util.Scanner;
  */
 public class Demo02RandomGame {
     public static void main(String[] args) {
-        System.out.print("请输入你猜的数：");
-        int tmp;
-
         // 产生随机数字只用一次,所以可以用匿名对象来完成
         int num = new Random().nextInt(101);
         Scanner scanner = new Scanner(System.in);
-        tmp = scanner.nextInt();
+
+
+        System.out.print("请选择猜数种类,1：无限次；2：有限次: ");
+        int type = scanner.nextInt();
+        if (type == 1) {
+            func1(scanner, num);
+        } else {
+            System.out.print("请输入猜数次数：");
+            int count = scanner.nextInt();
+            func2(scanner, num, count);
+        }
+    }
+
+    public static void func1(Scanner scanner, int num) {
+        System.out.print("请输入你猜的数：");
+        int tmp = scanner.nextInt();
         while (true) {
+            if (tmp > num) {
+                System.out.print("猜大了,请重新输入：");
+                tmp = scanner.nextInt();
+            } else if (tmp < num) {
+                System.out.print("猜小了,请重新输入：");
+                tmp = scanner.nextInt();
+            } else {
+                System.out.println("恭喜你,猜对了！");
+                break;
+            }
+        }
+    }
+
+    public static void func2(Scanner scanner, int num, int count) {
+        System.out.print("请输入你猜的数：");
+        int tmp = scanner.nextInt();
+        while (count > 1) {
+            count--;
             if (tmp > num) {
                 System.out.print("猜大了,请重新输入：");
                 tmp = scanner.nextInt();
