2e04f3384703090c88e31d37e3a15113459c750e
1325075688gw <1325075688@qq.com>
2019-10-30 11:23:06 +0800

    Object array.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

--- day07-code/src/com/biggw/day07/demo04/Demo01ArrayPerson.java
+++ day07-code/src/com/biggw/day07/demo04/Demo01ArrayPerson.java
@@ -6,6 +6,12 @@ import java.util.Arrays;
  * @author gw
  * @date 2019/10/30 0030 上午 11:12
  */
+
+/*
+* 对象数组的本质也是数组,也就是说数组长度一开始就确定了,程序运行区间不可改变
+*
+* 所以我们必须寻求一种可变长度的动态数组:ArrayList
+* */
 public class Demo01ArrayPerson {
     public static void main(String[] args) {
         Person[] array = new Person[3];
