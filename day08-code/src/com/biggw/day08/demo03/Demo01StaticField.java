7d665d27fe7fc4eae7cf14e1e6e477d0b2402f81
1325075688gw <1325075688@qq.com>
2019-10-30 22:38:18 +0800

    Static method and static variable.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

---
+++ day08-code/src/com/biggw/day08/demo03/Demo01StaticField.java
@@ -0,0 +1,17 @@
+package com.biggw.day08.demo03;
+
+/**
+ * @author gw
+ * @date 2019/10/30 0030 下午 22:29
+ */
+public class Demo01StaticField {
+    public static void main(String[] args) {
+        Student stu1 = new Student(23, "小白");
+        Student stu2 = new Student(20, "小黑");
+
+        stu1.setBanji("高三一班");
+        System.out.println("stu1.name: " + stu1.getName() + ", stu1.age: " + stu1.getAge() + ", stu1.班级：" + Student.getBanji() + ", stu1.id: " + stu1.getId());
+        System.out.println("stu2.name: " + stu2.getName() + ", stu2.age: " + stu2.getAge() + ", stu2.班级：" + Student.getBanji() + ", stu2.id: " + stu2.getId());
+
+    }
+}
---
+++ day08-code/src/com/biggw/day08/demo03/Student.java
@@ -0,0 +1,70 @@
+package com.biggw.day08.demo03;
+
+import java.awt.*;
+
+/**
+ * @author gw
+ * @date 2019/10/30 0030 下午 22:25
+ */
+
+/*
+ * static: （类变量）静态变量或者（类方法）静态方法
+ *
+ * */
+public class Student {
+    private int age;
+    private String name;
+    private int id;
+    private static String banji;
+    private static int numCounter;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+        this.id = ++numCounter;
+    }
+
+    public Student() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static String getBanji() {
+        return banji;
+    }
+
+    public static void setBanji(String banji) {
+        Student.banji = banji;
+    }
+
+    public static int getNumCounter() {
+        return numCounter;
+    }
+
+    public static void setNumCounter(int numCounter) {
+        Student.numCounter = numCounter;
+    }
+}
+
