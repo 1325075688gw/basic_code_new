6aa72044090dfbb2714d173a1c5e57dcf66927df
1325075688gw <1325075688@qq.com>
2019-10-31 18:26:16 +0800

    fixup! Static method can be called not using ClassName.StaticMethod when...
    Signed-off-by: 1325075688gw <1325075688@qq.com>

--- day08-code/src/com/biggw/day08/demo03/Demo01StaticField.java
+++ day08-code/src/com/biggw/day08/demo03/Demo01StaticField.java
@@ -9,7 +9,7 @@ public class Demo01StaticField {
         Student stu1 = new Student(23, "小白");
         Student stu2 = new Student(20, "小黑");
 
-        stu1.setBanji("高三一班");
+        Student.setBanji("高三一班");
         System.out.println("stu1.name: " + stu1.getName() + ", stu1.age: " + stu1.getAge() + ", stu1.班级：" + Student.getBanji() + ", stu1.id: " + stu1.getId());
         System.out.println("stu2.name: " + stu2.getName() + ", stu2.age: " + stu2.getAge() + ", stu2.班级：" + Student.getBanji() + ", stu2.id: " + stu2.getId());
 
