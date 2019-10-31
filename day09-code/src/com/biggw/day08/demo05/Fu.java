bf3a3ea3f24825fbe1dd5f897e46be628755a756
1325075688gw <1325075688@qq.com>
2019-10-31 20:32:05 +0800

    Override.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

---
+++ day09-code/src/com/biggw/day08/demo05/Fu.java
@@ -0,0 +1,11 @@
+package com.biggw.day08.demo05;
+
+/**
+ * @author gw
+ * @date 2019/10/31 0031 下午 20:23
+ */
+public class Fu {
+    public void method(){
+
+    }
+}
---
+++ day09-code/src/com/biggw/day08/demo05/Zi.java
@@ -0,0 +1,21 @@
+package com.biggw.day08.demo05;
+
+/**
+ * @author gw
+ * @date 2019/10/31 0031 下午 20:23
+ */
+
+/*
+ * 方法的覆盖重写：
+ *
+ * 1.子类和父类名称相同,参数列表也相同
+ * 2.@Override可以加可以不加,但是尽量加上,这样编译器可以帮我们检查是不是重写
+ * 3.子类方法的返回值必须【小于等于】父类方法的返回值  比如 Object String
+ * 4.子类方法的权限必须【大于等于】父类方法的权限修饰符 比如public pretected default private
+ * */
+public class Zi extends Fu {
+    @Override
+    public void method() {
+
+    }
+}
