f896e094f7bc57e3e4b25e14a87047f7ad2907d6
1325075688gw <1325075688@qq.com>
2019-10-31 21:24:32 +0800

    Constructor when extends.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

--- day09-code/src/com/biggw/day08/demo07/Zi.java
+++ day09-code/src/com/biggw/day08/demo07/Zi.java
@@ -8,12 +8,16 @@ public class Zi extends Fu {
     public Zi(int age) {
         // 默认隐含了一个父类的构造方法
         // super()
+
+        // 调用本类的无参构造函数,而且super和this构造函数只能同时用一个
+        this();
         System.out.println("子类带参构造函数");
     }
 
     public Zi() {
         // 默认隐含了一个父类的构造方法
         // super()
+
         System.out.println("子类无参数构造函数");
     }
 }
