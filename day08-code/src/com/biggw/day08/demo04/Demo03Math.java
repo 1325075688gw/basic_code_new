ab064e3947ef451554ba0adf7353c897e54448db
1325075688gw <1325075688@qq.com>
2019-10-31 19:15:04 +0800

    Math.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

---
+++ day08-code/src/com/biggw/day08/demo04/Demo03Math.java
@@ -0,0 +1,20 @@
+package com.biggw.day08.demo04;
+
+/**
+ * @author gw
+ * @date 2019/10/31 0031 下午 19:06
+ */
+
+/*
+ * java.util.Math 数学工具类
+ *
+ * public static double abs(double num); 求绝对值
+ * public static double ceil(double num); 向上取整
+ * public static double floor(double num); 向下取整
+ * public static long round(double num); 返回值不带小数点
+ *
+ * Math.PI
+ * */
+public class Demo03Math {
+
+}
