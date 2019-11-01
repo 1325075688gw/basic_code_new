5472cb8b880ef1b25f14cb820225587ab5f26bc9
1325075688gw <1325075688@qq.com>
2019-11-01 16:43:11 +0800

    polymorphic
    Signed-off-by: 1325075688gw <1325075688@qq.com>

---
+++ day10-code/src/com/biggw/day10/demo04/Main.java
@@ -0,0 +1,36 @@
+package com.biggw.day10.demo04;
+
+/**
+ * @author gw
+ * @date 2019/11/1 0001 下午 16:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        // 多态
+
+        /*
+         * 父类引用指向子类对象
+         *
+         * 父类名称 对象名 = new 子类名称();     extends
+         *
+         * 接口名称 对象名 = new 实现类名称();   implments
+         *
+         *
+         * 编译看左,运行看右
+         * */
+        Fu one = new Zi();
+        one.methodCommon();
+        one.methodFu();
+        System.out.println("=================");
+
+        Zi zi = new Zi();
+        zi.methodCommon();
+        zi.methodZi();
+        zi.methodFu();
+        System.out.println("=================");
+
+        Fu fu = new Fu();
+        fu.methodFu();
+        fu.methodCommon();
+    }
+}
---
+++ day10-code/src/com/biggw/day10/demo04/Zi.java
@@ -0,0 +1,15 @@
+package com.biggw.day10.demo04;
+
+/**
+ * @author gw
+ * @date 2019/11/1 0001 下午 16:32
+ */
+public class Zi extends Fu {
+    public void methodCommon(){
+        System.out.println("子类里面的方法");
+    }
+
+    public void methodZi(){
+        System.out.println("子类特有方法");
+    }
+}
