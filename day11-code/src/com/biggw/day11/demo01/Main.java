3ce769ae11dc62356e71e91d73a14818b5e086f1
1325075688gw <1325075688@qq.com>
2019-11-02 17:38:37 +0800

    final keyword.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

---
+++ day11-code/src/com/biggw/day11/demo01/Main.java
@@ -0,0 +1,26 @@
+package com.biggw.day11.demo01;
+
+/**
+ * @author gw
+ * @date 2019/11/2 0002 下午 17:14
+ */
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("高圆圆");
+        System.out.println("person.getName() = " + person.getName());
+        person = new Person("哈利波特");
+        System.out.println("person.getName() = " + person.getName());
+
+        final Person person1 = new Person("白展堂");
+        System.out.println("person1.getName() = " + person1.getName());
+
+        final int a;
+        a=3;
+
+        // 对于基本数据类型的变量,加上final后,则数据内容不能变
+        // 对于引用类型,加上final后,引用数据的地址不能变
+        // person1 = new Person("小怪兽"); 错误
+
+    }
+}
---
+++ day11-code/src/com/biggw/day11/demo01/Person.java
@@ -0,0 +1,29 @@
+package com.biggw.day11.demo01;
+
+/**
+ * @author gw
+ * @date 2019/11/2 0002 下午 17:16
+ */
+
+
+/*
+ * 只有局部变量才不会默认赋值,成员变量和类变量都会进行默认赋值,但是我们要注意,
+ * 一旦我们成员变量或者类变量变成了final,那么系统将不会提供默认赋值,所以我们需要手动赋值。
+ *
+ * 或者我们不进行手动赋值,但是我们的所有构造方法都必须对final的成员变量进行复制
+ * */
+public class Person {
+    String name = "高圆圆";
+    static final String localClass = "邯郸";
+    final String localMenber = "重庆";
+    final String localMenber2;
+
+    public Person(String name, String localMenber2) {
+        this.name = name;
+        this.localMenber2 = localMenber2;
+    }
+
+    public Person() {
+        this.localMenber2 = "佟大为";
+    }
+}
