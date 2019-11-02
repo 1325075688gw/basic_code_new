d74944c8498c838d0fa4ecefa97403ab328bb046
1325075688gw <1325075688@qq.com>
2019-11-02 21:53:41 +0800

    Anonymous Inner Class.
    Signed-off-by: 1325075688gw <1325075688@qq.com>

--- day11-code/src/com/biggw/day11/demo05/MyInterfaceMain.java
+++ day11-code/src/com/biggw/day11/demo05/MyInterfaceMain.java
@@ -7,7 +7,7 @@ package com.biggw.day11.demo05;
 
 /*
  * 匿名内部类
- * 如果接口的实现类（或者抽象父类的子类）只需要使用唯一得一次
+ * 如果接口的实现类（或者抽象父类的子类）只需要使用唯一得一次【实现类或者子类只用一次,但是生成的实现类对象【只能创建一个对象】,但是这个对象我们可以无限次使用,注意和匿名对象做好区别】
  * 那么这种情况下,我们就可以省略该类的定义,而改为使用匿名内部类【省略的其实就是新创建一个文件,实现接口或者实现子类】
  *
  * 匿名内部类的格式:
@@ -15,6 +15,8 @@ package com.biggw.day11.demo05;
  *      // 覆盖重写所有的抽象方法
  * }
  *
+ *
+ * 匿名内部类格式解析:
  * */
 
 public class MyInterfaceMain {
@@ -33,5 +35,12 @@ public class MyInterfaceMain {
         };
 
         myInterface2.method();
+
+        new MyInterface(){
+            @Override
+            public void method() {
+                System.out.println("匿名内部类,实现抽象方法,同时使用匿名对象");
+            }
+        }.method();
     }
 }
